dea8233361192b6054c4219591cace99519aa50b
criostasis <criostasis@gmail.com>
2024-04-29 17:26:27 +0000

    minor changes to create mar script and gitignore file to ignore dataset folder

--- LLM/Model/create_mar.sh
+++ LLM/Model/create_mar.sh
@@ -1,1 +1,5 @@
-torch-model-archiver --model-name wizardlmtest --version 1.0 --serialized-file wizardlm-13b-v1.2.Q4_0.gguf --extra-files "database_helper.py,HSU_index/index.faiss,HSU_index/index.pkl" --handler model_handler.py --export-path .
+#!/bin/bash
+
+rm wizardlmtest.mar
+
+torch-model-archiver --model-name wizardlmtest --version 1.0 --serialized-file wizardlm-13b-v1.2.Q4_0.gguf --extra-files "database_helper.py,index.faiss,index.pkl" --handler model_handler.py --export-path .
